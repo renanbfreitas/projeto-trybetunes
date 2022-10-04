7088b1a328b3f13810946286e6bc67a18e2aab67
Renanbfreitas <renanbf1992@outlook.com>
2022-10-03 22:47:30 -0300

    Adicionando item 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -20,6 +21,15 @@ class Header extends React.Component {
     const { loading, usuario } = this.state;
     return (
       <header data-testid="header-component">
+        <div>
+          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+        </div>
+        <div>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+        </div>
+        <div>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </div>
         {loading ? <Loading /> : <p data-testid="header-user-name">{ usuario }</p>}
       </header>
     );
