2cd115f0b4ece81f118989b137db9a6ccd076f73
Renanbfreitas <renanbf1992@outlook.com>
2022-10-04 19:32:27 -0300

    Adicionando item 6 e refatorando código

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,81 +1,88 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
-import { createUser } from '../services/userAPI';
-import Loading from '../components/Loading';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 
 class Search extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      disabledButton: true,
-      nome: '',
-      loading: false,
-      redirect: false,
-    };
-  }
-
-  formButton = ({ target }) => {
-    const validButton = 2;
-    if (target.value.length >= validButton) {
-      this.setState({ disabledButton: false, nome: target.value });
-    } else {
-      this.setState({ disabledButton: true, nome: target.value });
-    }
+  state = {
+    nome: '',
+    loading: false,
+    musicAlbum: [],
   };
 
-  validState = () => {
-    const { nome } = this.state;
-    const { history } = this.props;
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ nome: value });
+  };
 
-    this.setState(
-      { loading: true },
-      async () => {
-        await createUser({ name: nome });
-        this.setState({ loading: false });
-        history.push('/search');
-        console.log(history);
-      },
-    );
+  handleClick = () => {
+    const { nome } = this.state;
+    this.setState({ loading: true }, async () => {
+      const musicAlbum = await searchAlbumsAPI(nome);
+      this.setState({ musicAlbum, nome, loading: false });
+    });
   };
 
   render() {
-    const { disabledButton, loading, redirect } = this.state;
+    const {
+      nome,
+      musicAlbum,
+      loading,
+    } = this.state;
+    const VALIDBUTTON = 2;
+    const validInput = nome.length >= VALIDBUTTON;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <label htmlFor="name">
-            <input
-              data-testid="search-artist-input"
-              type="text"
-              name="name"
-              onChange={ this.formButton }
-              placeholder="Insira o nome do Artista ou os álbums de uma banda"
-            />
-          </label>
-          <button
-            data-testid="search-artist-button"
-            type="button"
-            disabled={ disabledButton }
-            onClick={ this.validState }
-          >
-            Pesquisar
-          </button>
-        </form>
-        { loading && <Loading /> }
-        {redirect && <Redirect to="/search" />}
+        {loading ? (<Loading />
+        ) : (
+          <div>
+            <form>
+              <label htmlFor="search">
+                <input
+                  type="text"
+                  placeholder="Nome do Artista"
+                  data-testid="search-artist-input"
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ !validInput }
+                onClick={ this.handleClick }
+              >
+                Pesquisar
+              </button>
+            </form>
+            <div>
+              {!musicAlbum.length ? 'Nenhum álbum foi encontrado' : (
+                <div>
+                  <h1>{`Resultado de álbuns de: ${nome}`}</h1>
+                  {musicAlbum.map((music) => (
+                    <div key={ music.collectionId }>
+                      <img src={ music.artworkUrl100 } alt={ music.artistName } />
+                      <h3>{music.artistName}</h3>
+                      <p>{music.collectionPrice}</p>
+                      <p>{music.releaseDate}</p>
+                      <p>{music.trackCount}</p>
+                      <Link
+                        to={ `/album/${music.collectionId}` }
+                        data-testid={ `link-to-album-${music.collectionId}` }
+                      >
+                        {music.collectionName}
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
-
-Search.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }).isRequired,
-};
 
 export default Search;
